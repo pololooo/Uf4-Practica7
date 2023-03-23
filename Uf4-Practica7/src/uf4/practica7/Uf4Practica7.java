543552d2dfa57b446eaf3766eb52a8cdb3f772a3
Pol García <Pol Garca@DESKTOP-DVLNGMA>
2023-03-23 13:29:01 +0100

    pol

---
+++ Uf4-Practica7/src/uf4/practica7/Uf4Practica7.java
@@ -0,0 +1,106 @@
+package uf4.practica7;
+
+public class Uf4Practica7 {
+
+    public static void main(String[] args) {
+
+    }
+}
+/*
+public class Estudiant {
+    private String dni;
+    private String nom;
+    private String cognom;
+    private double notaFinal;
+
+    public Estudiant(String dni, String nom, String cognom, double notaFinal) {
+        this.dni = dni;
+        this.nom = nom;
+        this.cognom = cognom;
+        this.notaFinal = notaFinal;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getCognom() {
+        return cognom;
+    }
+
+    public void setCognom(String cognom) {
+        this.cognom = cognom;
+    }
+
+    public double getNotaFinal() {
+        return notaFinal;
+    }
+
+    public void setNotaFinal(double notaFinal) {
+        this.notaFinal = notaFinal;
+    }
+
+    @Override
+    public String toString() {
+        return "Estudiant{" +
+                "dni='" + dni + '\'' +
+                ", nom='" + nom + '\'' +
+                ", cognom='" + cognom + '\'' +
+                ", notaFinal=" + notaFinal +
+                '}';
+    }
+}
+
+public class Professor {
+    private String nom;
+    private String cognom;
+    private String formacio;
+
+    public Professor(String nom, String cognom, String formacio) {
+        this.nom = nom;
+        this.cognom = cognom;
+        this.formacio = formacio;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getCognom() {
+        return cognom;
+    }
+
+    public void setCognom(String cognom) {
+        this.cognom = cognom;
+    }
+
+    public String getFormacio() {
+        return formacio;
+    }
+
+    public void setFormacio(String formacio) {
+        this.formacio = formacio;
+    }
+
+    @Override
+    public String toString() {
+        return "Professor{" +
+               
+
+*/
